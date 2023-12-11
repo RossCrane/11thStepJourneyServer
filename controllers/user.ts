efa98fdf8a8611394a14ee33b3ef6f26d7f3f700
knaughtt <dylanrcrane@gmail.com>
2023-12-10 20:06:28 -0700

    Cleaning up.

--- controllers/user.ts
+++ controllers/user.ts
@@ -217,16 +217,16 @@ export const getUsers = async (req: Request, res: Response): Promise<void> => {
 	}
 };
 
-export const getUsers = async (req: Request, res: Response): Promise<void> => {
-	try {
-		const users: IUser[] = await UserModel.find({}, '_id firstName');
+// export const getUsers = async (req: Request, res: Response): Promise<void> => {
+// 	try {
+// 		const users: IUser[] = await UserModel.find({}, '_id firstName');
 
-		res.json(users);
-	} catch (error) {
-		console.error(error);
-		res.status(500).json({ error: 'Error fetching users' });
-	}
-};
+// 		res.json(users);
+// 	} catch (error) {
+// 		console.error(error);
+// 		res.status(500).json({ error: 'Error fetching users' });
+// 	}
+// };
 
 //Blacklist for Dylan's reference
 export async function logout(
